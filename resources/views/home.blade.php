03cc1a4fb358dff0577bca07fa1c6ce2f54a09b4
imantarico <icoversi90@gmail.com>
2025-02-06 18:15:09 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SIPOLGAN</title>
+    <!-- Tambahkan CDN Tailwind jika belum terhubung -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="flex items-center justify-center h-screen bg-gray-100">
+<div class="text-center">
+    <img src="{{ asset('img/logo.png') }}" alt="logo" class="w-3/4 mx-auto">
+</div>
+</body>
+</html>
